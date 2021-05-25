23ee1a8645c85f0ddfa86ff7cf12bb46072561cd
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2021-05-25 13:25:33 +0300

    incapsulation ref

--- js/index.js
+++ js/index.js
@@ -26,7 +26,6 @@ class CountdownTimer{
 
     getTimeComponents(time) {
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((time % (1000 * 60)) / 1000);
